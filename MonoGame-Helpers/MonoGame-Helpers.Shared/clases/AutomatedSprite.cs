777e19b0be62e79aafc3ce886004f09dff62e5f0
Victor Gaete <victor.gaete.flores@gmail.com>
2015-06-16 23:03:38 -0400

    clases Automated Sprite,sprite y usercontrolledSprite

---
+++ MonoGame-Helpers/MonoGame-Helpers.Shared/clases/AutomatedSprite.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoGame_Helpers.clases
+{
+    class AutomatedSprite : Sprite
+    {
+        public override Vector2 direction
+        {
+            get { return speed; }
+        }
+
+        public AutomatedSprite(Texture2D textureImage, Vector2 position, Point frameSize,
+int collisionOffset, Point currentFrame, Point sheetSize, Vector2 speed)
+            : base(textureImage, position, frameSize, collisionOffset, currentFrame,
+            sheetSize, speed)
+        { }
+
+        public AutomatedSprite(Texture2D textureImage, Vector2 position, Point frameSize,
+int collisionOffset, Point currentFrame, Point sheetSize, Vector2 speed,
+int millisecondsPerFrame)
+            : base(textureImage, position, frameSize, collisionOffset, currentFrame,
+            sheetSize, speed, millisecondsPerFrame)
+        { }
+
+        public override void Update(GameTime gameTime, Rectangle clientBounds)
+        {
+            position += direction;
+            base.Update(gameTime, clientBounds);
+        }
+    }
+}
