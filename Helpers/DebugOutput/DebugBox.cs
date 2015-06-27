8f5d1302335d057881da8c789e3bf48713d22fee
Esteban Gaete <zoraskiller@gmail.com>
2015-06-27 19:40:29 -0400

    CHANGE/ADD: DebugOutput en su propio namespace + debug box para outputs

---
+++ Helpers/DebugOutput/DebugBox.cs
@@ -0,0 +1,71 @@
+﻿using Helpers.Polygon;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Helpers.ExtensionMethods.Vector2Ext;
+
+namespace Helpers.DebugOutput
+{
+    class DebugBox
+    {
+        private List<string> messages;
+        private DrawableRectangle rect_background;
+        private Vector2 rect_position;
+        private SpriteFont Font;
+        private bool left_corner;
+        private Vector2 msg_bounds;
+        private const int POS_OFFSET = 10;
+        private const float RECT_ALPHA = 0.5f;
+        private const int LINE_SPACING = 5;
+
+        public DebugBox(Vector2 position, SpriteFont font, bool left_corner, params string[] messages)
+        {
+            this.rect_position = position;
+            this.Font = font;
+            this.left_corner = left_corner;
+            this.msg_bounds = Vector2.Zero;
+            this.messages = messages.ToList();          
+        }
+
+        public void LoadContent(GraphicsDevice graphicsDevice)
+        {
+            this.msg_bounds.X = this.messages.Max(s => this.Font.MeasureString(s).X);
+            this.msg_bounds.Y = this.Font.MeasureString(this.messages[0]).Y;
+
+            if (!this.left_corner)
+                this.rect_position = this.rect_position - new Vector2(this.msg_bounds.X + 10, 0);
+
+            this.rect_background = new DrawableRectangle(
+                this.rect_position,
+                (int)this.msg_bounds.X + POS_OFFSET,
+                (int)(this.messages.Count * (LINE_SPACING + this.msg_bounds.Y) - LINE_SPACING + POS_OFFSET)
+            );
+            this.rect_background.Alpha = RECT_ALPHA;
+            this.rect_background.LoadContent(graphicsDevice);
+        }
+
+        public void Update(params string[] messages)
+        {
+            this.messages = messages.ToList();
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            this.rect_background.Draw(gameTime, spriteBatch);
+
+            for (int i = 0; i < this.messages.Count; i++)
+            {
+                spriteBatch.DrawString(
+                    this.Font,
+                    this.messages[i], 
+                    this.rect_position.Add(POS_OFFSET / 2.0f) + new Vector2(0, i*(msg_bounds.Y + LINE_SPACING)),
+                    Color.White
+                );
+            }
+        }
+    }
+}
---
+++ Helpers/DebugOutput/DebugOutput.cs
@@ -0,0 +1,91 @@
+﻿using Helpers.Polygon;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Helpers.ExtensionMethods.Vector2Ext;
+
+namespace Helpers.DebugOutput
+{
+    public class DebugOutput : DrawableGameComponent
+    {
+        private ContentManager content;
+        private SpriteBatch spriteBatch;
+        private SpriteFont debug_font;
+
+        private RenderTarget2D rectangles;
+
+        private DebugBox fpsBox;
+        private DebugBox resolutionBox;
+
+        #region Object Counter
+
+        #endregion
+
+        #region Assets Counter
+
+        #endregion
+
+
+        public DebugOutput(Game game)
+            : base(game) { }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            if (content == null)
+                content = new ContentManager(Game.Services, "Content");
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            debug_font = content.Load<SpriteFont>("Fonts/DebugFont");
+
+            this.fpsBox = new DebugBox(new Vector2(10, 10), debug_font, true, "FPS: 60");
+            this.fpsBox.LoadContent(GraphicsDevice);
+
+            this.resolutionBox = new DebugBox(new Vector2(Game.Window.ClientBounds.Width - 10, 10), debug_font, false, "Resolution:","1920x1080");
+            this.resolutionBox.LoadContent(GraphicsDevice);
+
+            base.LoadContent();
+        }
+
+        protected override void UnloadContent()
+        {
+            content.Unload();
+
+            base.UnloadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            // Capturando datos para FPS
+            this.fpsBox.Update(string.Format("FPS: {0}", 1000 / gameTime.ElapsedGameTime.Milliseconds));
+
+            // Capturando datos para Resolution
+            this.resolutionBox.Update("Resolution",string.Format("{0}x{1}", Game.Window.ClientBounds.Width, Game.Window.ClientBounds.Height));
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+
+            spriteBatch.Begin();
+
+            // Dibujando FPS
+            this.fpsBox.Draw(gameTime, spriteBatch);
+
+            // Dibujando Resolution
+            this.resolutionBox.Draw(gameTime, spriteBatch);
+
+            spriteBatch.End();
+        }
+    }
+}
