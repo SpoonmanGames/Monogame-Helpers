42dac9e9351024f207afffa54b7634234fb76d5d
Esteban Gaete <zoraskiller@gmail.com>
2015-06-27 17:44:37 -0400

    NEW: Namespace para extensiones + extensiones de Vector2

---
+++ Helpers/ExtensionMethods/Vector2Ext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Helpers.ExtensionMethods.Vector2Ext
+{
+    public static class Vector2Ext
+    {
+        public static Vector2 Add(this Vector2 v2, int value2)
+        {
+            v2.X += value2;
+            v2.Y += value2;
+            return v2;
+        }
+
+        public static Vector2 Add(this Vector2 v2, float value2)
+        {
+            v2.X += value2;
+            v2.Y += value2;
+            return v2;
+        }
+    }
+}
