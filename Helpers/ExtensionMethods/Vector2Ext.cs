e4d71d6528277d0fa7ea3c887b3c1665f36335d0
Esteban Gaete <zoraskiller@gmail.com>
2015-06-27 20:19:38 -0400

    ADD: Add methods a Vector2

--- Helpers/ExtensionMethods/Vector2Ext.cs
+++ Helpers/ExtensionMethods/Vector2Ext.cs
@@ -17,5 +17,29 @@ namespace Helpers.ExtensionMethods.Vector2Ext
             v2.Y += value2;
             return v2;
         }
+
+        public static Vector2 AddToX(this Vector2 v2, int value2)
+        {
+            v2.X += value2;
+            return v2;
+        }
+
+        public static Vector2 AddToX(this Vector2 v2, float value2)
+        {
+            v2.X += value2;
+            return v2;
+        }
+
+        public static Vector2 AddToY(this Vector2 v2, int value2)
+        {
+            v2.Y += value2;
+            return v2;
+        }
+
+        public static Vector2 AddToY(this Vector2 v2, float value2)
+        {
+            v2.Y += value2;
+            return v2;
+        }
     }
 }
